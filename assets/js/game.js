7addb2d1c10862edcdab36be58aaba63426f8a66
Husky <rarity322@gmail.com>
2020-12-21 19:14:22 -0700

    added skip and playerMoney

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 var enemyName = "Roborto";
 var enemyHealth = 50;
@@ -9,6 +10,10 @@ var enemyAttack = 12;
 var fight = function() {
     // Alert players that they are starting the round 
     window.alert("Welcome to Robot Gladiators!");
+
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to chose.");
+
+    if (promptFight === "fight" || promptFight === "FIGHT") {
 
     //Subtract the value of playerAttack from the value of enemyHealth and use that result to update the value in the enemyHealth variable
     enemyHealth = enemyHealth - playerAttack;
@@ -21,8 +26,7 @@ var fight = function() {
     // check enemy's health
     if (enemyHealth <= 0) {
         window.alert(enemyName + " has died!");
-    }
-    else {
+    }else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
 
@@ -37,10 +41,25 @@ var fight = function() {
     // check player's health 
     if (playerHealth <= 0) {
         window.alert(Playername + " has died!");
-    }
-    else {
+    } else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
+    // if player choses to skip
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+    // confirm player wants to skip 
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        // subtract money from PlayerMoney for skipping 
+        playerMoney = playerMoney - 2;
+    }
+    // if no (false), ask question again by running fight() again
+    else {
+        fight();
+    }
+}
 };
 
 fight();
